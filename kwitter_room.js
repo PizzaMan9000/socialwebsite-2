1810fae7aae9b52add1f2e678447b351455050e7
PizzaMan9000 <88388990+PizzaMan9000@users.noreply.github.com>
2021-12-13 17:36:15 +0300

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBacOpXkHAZbZ0s2ZqbIlY6hxPzUCGfXd0",
+    authDomain: "chatappproject-2f098.firebaseapp.com",
+    databaseURL: "https://chatappproject-2f098-default-rtdb.firebaseio.com",
+    projectId: "chatappproject-2f098",
+    storageBucket: "chatappproject-2f098.appspot.com",
+    messagingSenderId: "83926784973",
+    appId: "1:83926784973:web:3e14225152a7a049f40284"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
